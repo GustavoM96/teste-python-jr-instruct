9ab5afa5fe2529732f59353d6ae18d186a99a752
Gustavo Messias <gustavo.hmessias96@gmail.com>
2021-08-10 14:26:29 -0300

    feat: add services create_project and create_package_list

--- api/services.py
+++ api/services.py
@@ -7,38 +7,50 @@ class ApiServices:
     def is_validated_package(package: dict) -> bool:
         host_pypi = "https://pypi.org"
 
-        if package.version:
+        if package.get("version"):
             response = requests.get(
                 f'{host_pypi}/pypi/{package["name"]}/{package["version"]}/json'
             )
         else:
             response = requests.get(f'{host_pypi}/pypi/{package["name"]}/json')
 
-        if response.status_code == "200":
+        if response.status_code == 200:
             return True
-
         return False
 
     @staticmethod
-    def get_latest_version_package(package: dict) -> dict:
+    def get_latest_version_package(package: dict) -> str:
         host_pypi = "https://pypi.org"
 
-        response = requests.get(f"{host_pypi}/pypi/{package.name}/json")
-        lastest_version = response.text.info.version
+        response = requests.get(f'{host_pypi}/pypi/{package["name"]}/json')
+        lastest_version = response.json()["info"]["version"]
 
         return lastest_version
 
     @classmethod
-    def crate_project(cls, project_name: str, package_list: list[dict]):
-        project = Project.objects.create(name=project_name)
+    def create_package_list(cls, package_list: list[dict]) -> list[PackageRelease]:
+        created_package_list = []
 
         for package in package_list:
             if not cls.is_validated_package(package):
                 raise ValueError({"error": "One or more packages don't exist"})
 
             package["version"] = cls.get_latest_version_package(package)
+            package = PackageRelease.objects.get_or_create(**package)[0]
 
-        created_package_list = PackageRelease.objects.bulk_create(package_list)
+            created_package_list.append(package)
+
+        return created_package_list
+
+    @classmethod
+    def create_project(cls, project_data) -> Project:
+        project, is_created = Project.objects.get_or_create(name=project_data["name"])
+
+        if not is_created:
+            raise ValueError({"error": "project already exists"})
+
+        created_package_list = cls.create_package_list(project_data["packages"])
+
         project.packages.set(created_package_list)
 
         return project
--- api/tests/tests_services.py
+++ api/tests/tests_services.py
@@ -1,9 +1,9 @@
+import requests
 from api.services import ApiServices
 from api.apps import ApiConfig
 from django.test import TestCase
 from rest_framework.test import APIClient
 from ..models import PackageRelease, Project
-from ipdb import set_trace
 
 # (1)
 
@@ -11,46 +11,54 @@ from ipdb import set_trace
 class ProjectServiceTest(TestCase):
     def setUp(self):
         self.client = APIClient()
-        self.project = Project.objects.create(name="Titan")
 
-        self.package1 = PackageRelease.objects.create(name="Django", version="1.0")
-        self.package2 = PackageRelease.objects.create(name="Flask", version="2.0")
+        self.package1 = dict(name="Django")
+        self.package2 = dict(name="Flask", version="2.0")
+        self.package_list = [self.package1, self.package2]
+        self.project = dict(name="Titan", packages=[self.package1, self.package2])
 
-        self.invalid_package_name = PackageRelease.objects.create(
-            name="laslpfuanfgs", version="1.0"
-        )
-        self.invalid_package_version = PackageRelease.objects.create(
-            name="Django", version="okdfsduhanca"
-        )
+        self.invalid_package_name = dict(name="laslpfuanfgs", version="1.0")
+        self.invalid_package_version = dict(name="Django", version="okdfsduhanca")
 
         self.host_pypi = "https://pypi.org"
 
-    # def test_register_project_valid(self):
-    #     project = ApiServices.crate_project(self.project.name,)
-    #     self.assertEqual(len(self.project.packages.all()), 2)
-
-    def test_invalid_package_list(self):
-        is_valid_name = ApiServices.is_validated_package_list(
-            [self.invalid_package_name]
-        )
-        is_valid_version = ApiServices.is_validated_package_list(
-            [self.invalid_package_version]
+    def test_service_is_validated_package_with_invalid_package(self):
+        is_valid_name = ApiServices.is_validated_package(self.invalid_package_name)
+        is_valid_version = ApiServices.is_validated_package(
+            self.invalid_package_version
         )
 
         self.assertEqual(is_valid_name, False)
         self.assertEqual(is_valid_version, False)
 
-    def test_valid_package_list(self):
-        is_valid_name = ApiServices.is_validated_package_list([self.package1])
-        is_valid_version = ApiServices.is_validated_package_list([self.package2])
+    def test_service_is_validated_package_with_valid_package(self):
+        is_valid_name = ApiServices.is_validated_package(self.package1)
+        is_valid_version = ApiServices.is_validated_package(self.package2)
 
         self.assertEqual(is_valid_name, True)
         self.assertEqual(is_valid_version, True)
 
-    def test_get_latest_version_package(self):
-        lastest_version = ApiServices.get_latest_version_package()
+    def test_service_get_latest_version_package(self):
+        lastest_version1 = ApiServices.get_latest_version_package(self.package1)
+        lastest_version2 = ApiServices.get_latest_version_package(self.package2)
 
-        response = self.client.get(f"{host_pypi}/pypi/{package.name}/json")
-        lastest_version_expected = response.json().info.version
+        response1 = requests.get(f'{self.host_pypi}/pypi/{self.package1["name"]}/json')
+        response2 = requests.get(f'{self.host_pypi}/pypi/{self.package2["name"]}/json')
 
-        self.assertEqual(lastest_version, lastest_version_expected)
+        lastest_version_expected1 = response1.json()["info"]["version"]
+        lastest_version_expected2 = response2.json()["info"]["version"]
+
+        self.assertEqual(lastest_version1, lastest_version_expected1)
+        self.assertEqual(lastest_version2, lastest_version_expected2)
+
+    def test_service_create_project_with_2_packages(self):
+        project = ApiServices.create_project(self.project)
+        quantity_package_expected = 2
+        self.assertEqual(len(project.packages.all()), quantity_package_expected)
+
+    def test_service_create_package_list(self):
+        package_list = ApiServices.create_package_list(self.package_list)
+        package_list_1 = PackageRelease.objects.get(name=self.package1["name"])
+        package_list_2 = PackageRelease.objects.get(name=self.package2["name"])
+        package_list_expected = [package_list_1, package_list_2]
+        self.assertListEqual(package_list, package_list_expected)
